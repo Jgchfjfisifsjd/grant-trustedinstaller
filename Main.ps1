35711eeb644b949e1b8331106d447bbe6409d5f0
Oscar Hacker Liu <89288836+Jgchfjfisifsjd@users.noreply.github.com>
2022-06-18 10:18:09 +0800

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -7,6 +7,7 @@ cd\
 c:
 remove-item token
 md token
+$sp = read-host "enter program name"
 save-module -name ntobjectmanager -path c:\token
 install-module -name ntobjectmanager
 set-ExecutionPolicy unrestricted
@@ -14,4 +15,6 @@ import-module ntobjectmanager
 sc.exe start TrustedInstaller
 Set-NtTokenPrivilege SeDebugPrivilege
 $p = Get-NtProcess -Name TrustedInstaller.exe
-$proc = New-Win32Process cmd.exe -CreationFlags NewConsole -ParentProcess $p
+$proc = New-Win32Process $sp -CreationFlags NewConsole -ParentProcess $p
+write-host "complete!"
+pause
